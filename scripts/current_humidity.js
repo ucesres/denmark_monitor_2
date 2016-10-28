b8ec93c2a12d05eaad5b5bb9d801ce5653549f89
Ed Sharp <ed.sharp@ucl.ac.uk>
2016-10-28 15:25:26 +0100

    web1

--- scripts/current_humidity.js
+++ scripts/current_humidity.js
@@ -1,19 +1,16 @@
-											
-var humid1 =Papa.parse("https://dl.dropboxusercontent.com/u/4076462/humidity_basement_latest_value.csv", {
+// load current humidity values pushed from pi every ten mins											
+var humid1 =Papa.parse("humidity_basement_latest_value.csv", {
 	download: true,
 	complete: function(results) {
-		console.log(results.data[1][1]);
-		console.log(results);
 		var str2 = "%"
 		document.getElementById("live-humid").innerHTML = results.data[1][1].concat(str2); 		
 	}
 });
+// load current temperature values pushed from pi every ten mins											
 
 var temp1 =Papa.parse("https://dl.dropboxusercontent.com/u/4076462/temperature_basement_latest_value.csv", {
 	download: true,
 	complete: function(results) {
-		console.log(results.data[1][1]);
-		console.log(results);
 		var str1 = " o"
 		var str1a = str1.sup()
 		var str2 = "C"
@@ -21,19 +18,29 @@ var temp1 =Papa.parse("https://dl.dropboxusercontent.com/u/4076462/temperature_b
 		document.getElementById("live-temp").innerHTML = results.data[1][1].concat(str3); 		
 	}
 });
-
+// load external conditions pushed from pi every hour after being called from api											
 var humid_out =Papa.parse("https://dl.dropboxusercontent.com/u/4076462/current_external_conditions.csv", {
 	download: true,
 	complete: function(results) {
-		console.log(results.data[1][3]);
-		console.log(results.data[1][3]);
+		
 		var str2a = "%"
 		document.getElementById("outside-humid").innerHTML = results.data[1][3].concat(str2a); 
 		var str1 = " o"
 		var str1a = str1.sup()
 		var str2 = "C"
 		var str3 = str1a.concat(str2)
-		document.getElementById("outside-temp").innerHTML = results.data[1][4].concat(str3); 		
+		document.getElementById("outside-temp").innerHTML = results.data[1][4].concat(str3); 	
+		
 	}
 });
 
+// load text descibing whether it is warmer and more humid outside
+var humid_out =Papa.parse("https://dl.dropboxusercontent.com/u/4076462/compare_conditions.csv", {
+	download: true,
+	complete: function(results) {
+		console.log
+		document.getElementById("compare-temp").innerHTML = results.data[1][2]; 	
+		document.getElementById("compare-humid").innerHTML = results.data[1][3]; 	
+
+	}
+});
